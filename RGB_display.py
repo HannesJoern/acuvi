ae248694be6d4ce03ef43e3ed05da394360ace3f
EmanueleHarrasser <85946327+EmanueleHarrasser@users.noreply.github.com>
2022-05-09 17:20:03 +0200

    Update RGB_display.py

--- RGB_display.py
+++ RGB_display.py
@@ -6,8 +6,9 @@ import scipy.fftpack
 import numpy as np
 
 class RGB_display:
-    h = 0.8
-    equ_frequqencystep=[218.75,437.5,875,1750,3500,7000]
+    h = 0.8 #size of window given in percentage relative to the screen size
+    equ_frequqencystep=[218.75,437.5,875,1750,3500,7000] # range of frequency bands in fft-visualiation
+    
     def __init__(self,x_squares:int,y_squares:int):
         self.x_squares = x_squares
         self.y_squares = y_squares
@@ -26,7 +27,7 @@ class RGB_display:
         self.canvas = tk.Canvas(master=self.master, height=self.height, width=self.width, bd=0)
 
 
-
+    #creates and opens the window
     def createRgbDisplay(self):
         for y in range(self.y_squares):
             self.squares_coordinates.append([])
@@ -35,29 +36,32 @@ class RGB_display:
                 corner_y = y*self.square_side
                 self.squares_coordinates[y].append(self.canvas.create_rectangle(corner_x, corner_y, corner_x + self.square_side, corner_y + self.square_side, fill="white",outline='black'))
         self.canvas.pack()
-
+    
+    # colors the square with coordinates (line_re,column_nr), the most top left square is on line_nr:0 and column_nr:0
     def colorSquare(self,line_nr:int,column_nr:int,red:int,green:int,blue:int):
         colorval = "#%02x%02x%02x" % (red, green, blue)
         self.canvas.itemconfig(self.squares_coordinates[line_nr][column_nr], fill=colorval)
-
+    
+    #sets color of all squares to white
     def clearSquares(self):
         for x in self.squares_coordinates:
             for y in x:
                 self.canvas.itemconfig(y,fill='white')
-
+    
+    #updates the window and shows all changes
     def update(self):
         self.master.update()
-
+    
+    #displays the fft transformation in bands of different frequency ranges specified in equ_frequencystep 
     def fftDisplay(self, sampleRate: int, soundData: [], sampleLength: int):
 
         combined = []
         for i in range(int(len(soundData) / 2)):
             combined.append(int((soundData[i * 2] + soundData[(i * 2) + 1]) / 2))
         yf = scipy.fftpack.rfft(combined)
-        #xf = scipy.fftpack.rfftfreq(1536,1/48000)
+
         step = 1/(sampleLength/sampleRate)
-        #plt.plot(xf,abs(yf))
-        #plt.show()
+        
 
 
         value = 0
@@ -80,7 +84,9 @@ class RGB_display:
                     self.colorSquare(12-h, (i * 3)+1, 0, 204, 204)
                     self.colorSquare(12-h, (i * 3)+2, 0, 204, 204)
         self.update()
-
+    
+    #visualizes the drums, input data is spleeter drums result, anly length works
+    #this only works with a window length of 24 squares
     def drums(self,soundData: []):
         intensity = max(soundData)
         for i in range(11):
@@ -95,7 +101,8 @@ class RGB_display:
                 self.colorSquare(self.y_squares - 1, 12 + i, 255, 255, 255)
                 self.colorSquare(self.y_squares - 2, 12 + i, 255, 255, 255)
 
-
+    
+    #this shit is just plain broken, use if you want to summon satan
     def bass(self,soundData: []):
         combined = []
         for i in range(int(len(soundData) / 2)):
