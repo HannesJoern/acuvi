4965d9cf751fe8f24f17da000b02ab540797f40b
HannesJoern <79064103+HannesJoern@users.noreply.github.com>
2022-05-19 18:21:35 +0200

    Add files via upload

---
+++ acuvi.py
@@ -0,0 +1,108 @@
+
+import multiprocess
+from multiprocess import Process, Queue
+import time as tm
+import numpy as np
+import math
+from sharedFunctions import *
+import audioIO
+
+import matplotlib.pyplot as plt
+
+
+
+stream_flag = 0 #this will be used to eventually exit the program! maybe... someday
+framelength = 10 #size of rotating array to store audio and visual data (buffer of ~4 is necessary, so we use 10)
+audio_output_counter = 0 #number of chunk when audio gets in
+audio_input_counter = 0 #number of chunk when it gets out, corresponds to position in vis_frame
+
+#experimental value that works for me
+delay = 0.4
+
+#basic settings (program might still break when changing these)
+RATE=44100
+CHUNKTIME = 10
+CHUNKSIZE = RATE*CHUNKTIME
+FPS = 30
+NUM_PIXELS = 300
+
+#vis_frame is a rotating array of size 10, in which the vis_samples are stored in chunks of size FPS*CHUNKTIME
+vis_frame = np.array([[["0x000000" for j in range(NUM_PIXELS)] for k in range(FPS*CHUNKTIME)] for l in range(framelength)])
+
+from audioWorker import audioWorker
+
+
+def main():
+    #necessary to prevent multiprocessing from taking over:
+    multiprocess.freeze_support()
+
+    #imports need to be here because of bug in libusb device access
+    #initialization of LEDs via USB SPI chip
+    import board
+    import neopixel_spi as neopixel
+
+    PIXEL_ORDER = neopixel.GRB
+    spi = board.SPI()
+
+    pixels = neopixel.NeoPixel_SPI(spi, NUM_PIXELS, pixel_order=PIXEL_ORDER, auto_write=False)
+
+    #these queues are necessary to send and receive data across processes (they are special multiprocessing queues)
+    audio_in_queue = Queue()
+    visual_data_queue = Queue()
+    audio_out_queue = Queue()
+    output_start_time = Queue()
+
+    #initialize audio IO
+    audio_io = audioIO.AudioIO(RATE, CHUNKTIME, CHUNKSIZE, framelength, audio_in_queue, audio_out_queue, visual_data_queue, output_start_time)
+    #start IO process (it will create input and output audio streams)
+    audio_io_worker = Process(target=audio_io.ioWorker, args=())
+    audio_io_worker.start()
+
+    #start spleeter and visualization process
+    audio_worker = Process(target=audioWorker, args=(audio_in_queue, visual_data_queue, audio_out_queue, RATE, CHUNKSIZE, CHUNKTIME, FPS, NUM_PIXELS))
+    audio_worker.start()
+
+    #we wait until audio output has started to get initial audio output start time
+    while(output_start_time.empty()):
+        tm.sleep(0.5)
+    initial_output_start_time = output_start_time.get()
+
+    #start the visualization loop
+    while True: 
+        try:   
+            #get visual data from processing queue
+            while not visual_data_queue.empty():
+
+                visualization, vis_counter = visual_data_queue.get()
+                vis_frame[vis_counter + 1] = visualization
+                print("vis data " + str(vis_counter) + " came in!")
+
+            #calculate position in frame (audio_output_counter) and in chunk (position)
+            current_time = tm.time()
+            audio_output_counter = int(math.floor((current_time - initial_output_start_time) / 10) )
+            position = int(((current_time + delay - initial_output_start_time) % 10) * FPS)
+
+            #precaution in case of bugs
+            if position < 0 or position > FPS*CHUNKTIME:
+                print("position corrected to fit into array" + str(position))
+                position = 0                         
+            
+            #obtain vis_sample
+            vis_sample = vis_frame[audio_output_counter][position]
+            
+            #write vis_sample to LEDs
+            for i in range(300):
+                pixels[i] = int(vis_sample[i], 16)
+
+            pixels.show()
+            
+        #this was intended to exit the program but doesnt work
+        except KeyboardInterrupt:
+            audio_io.terminate()
+            audio_worker.join()
+
+
+#somehow necessary i dont know python
+if __name__ == '__main__':
+    multiprocess.freeze_support()
+    main()
--- audioWorker.py
+++ audioWorker.py
@@ -1,161 +1,46 @@
-import sys
 import time as tm
-import numpy as np
-import resampy
-import spleeter
-from spleeter.separator import Separator
-from spleeter.audio.adapter import AudioAdapter
-import crepe
-from scipy.io.wavfile import write
-import math
 
-def audioWorker(mp_queue, mp_queue_vis, mp_queue_audio, RATE, CHUNKSIZE, CHUNKTIME, FPS):
-    separator = Separator('spleeter:4stems')
-    audio_loader = AudioAdapter.default()
+import audioProcessor
+import visualizer
+
+
+
+def audioWorker(audio_in_queue, visual_data_queue, audio_out_queue, RATE, CHUNKSIZE, CHUNKTIME, FPS, NUM_PIXELS):
+
+    audio_processor = audioProcessor.AudioProcessor(RATE, CHUNKSIZE, CHUNKTIME)
+    visual_processor = visualizer.Visualizer(RATE, CHUNKSIZE, CHUNKTIME, FPS, NUM_PIXELS)
+
     while(True):
-        while mp_queue.empty():
+        #wait for callback-in function to deliver us new data
+        while audio_in_queue.empty():
             tm.sleep(0.1)
         
+        #performance counter to see how fast our entire data processing is
         time_begin = tm.perf_counter()
         
-        data = np.array([[0 for i in range(2)] for j in range(CHUNKSIZE)])
-        byte_data, audio_input_counter = mp_queue.get()
-        np_data = np.frombuffer(byte_data, dtype=np.int16)
-        waveform = np.reshape(np_data, (CHUNKSIZE, 2))
+        #get data from audio input stream
+        byte_data, audio_input_counter = audio_in_queue.get()
 
-        print("audio worker started with audio_input_counter")
-        print(audio_input_counter)
-        prediction = separator.separate(waveform) 
+        print("audio worker started with audio_input_counter" + str(audio_input_counter))
+
+        #use spleeter
+        prediction = audio_processor.separate(byte_data)
         
-        vocals = prediction['vocals']
-        bass = prediction['bass']
-        drums = prediction['drums']
-        other = prediction['other']
+        #visualize
+        visualization = visual_processor.visualize(prediction)
 
-        #for now only once channnel of result is considered
+        #give audio output available to output stream
+        audio_out_queue.put([byte_data, audio_input_counter])
 
-        vocals_mono = vocals[:,0]# / 2 + vocals[:,1] / 2
-        bass_mono = bass[:,0]# / 2 + bass[:,1] / 2
-        drums_mono = drums[:,0] #/ 2 + drums[:,1] / 2
-        other_mono = other[:,0]# / 2 + other[:,1] / 2
+        #give visual data avialable to display
+        visual_data_queue.put([visualization, audio_input_counter])
 
-        #maybe unnecessary conversions found here
-
-        vocals_mono_abs = np.abs(vocals_mono)
-        bass_mono_abs = np.abs(bass_mono)
-        other_mono_abs = np.abs(other_mono)
-        drums_mono_abs = np.abs(drums_mono)
-
-        vocals_downsampled_temp = np.zeros(300)
-        bass_downsampled_temp = np.zeros(300)
-        other_downsampled_temp = np.zeros(300)
-        drums_downsampled_temp = np.zeros(300)
-        for i in range(300):
-            vocals_downsampled_temp[i] = np.sum(vocals_mono_abs[i*1470:(i+1)*1470-1])
-            bass_downsampled_temp[i] = np.sum(bass_mono_abs[i*1470:(i+1)*1470-1])
-            other_downsampled_temp[i] = np.sum(other_mono_abs[i*1470:(i+1)*1470-1])
-            drums_downsampled_temp[i] = np.sum(drums_mono_abs[i*1470:(i+1)*1470-1])
-
-        vocals_downsampled = vocals_downsampled_temp/1470
-        bass_downsampled = bass_downsampled_temp/1470
-        other_downsampled = other_downsampled_temp/1470
-        drums_downsampled = drums_downsampled_temp/1470
-
-        #pack up data for visualizer:
-
-        vis_data = np.zeros(4)
-        vis_data = np.array([vocals_downsampled, bass_downsampled, other_downsampled, drums_downsampled])
-        
-        #make data available for other processes:
-
-        mp_queue_audio.put([byte_data, audio_input_counter])
-        mp_queue_vis.put([vis_data, audio_input_counter])
-
-        
-        #old code that might be recycled:
-        """vocals_time, vocals_frequency, vocals_confidence, vocals_activation = crepe.predict(vocals_mono, RATE, model_capacity='tiny', viterbi=True)
-        other_time, other_frequency, other_confidence, other_activation = crepe.predict(other_mono, RATE, model_capacity='tiny', viterbi=True)
-        bass_time, bass_frequency, bass_confidence, bass_activation = crepe.predict(bass_mono, RATE, model_capacity='tiny', viterbi=True)
-        
-        
-        
-        
-        vocals_intensity_temp = 0
-        other_intensity_temp = 0
-        bass_intensity_temp = 0
-        drums_intensity_temp = 0
-        
-        vocals_intensity = np.zeros(FPS*CHUNKTIME)
-        other_intensity = np.zeros(FPS*CHUNKTIME)
-        bass_intensity = np.zeros(FPS*CHUNKTIME)
-        drums_intensity = np.zeros(FPS*CHUNKTIME)
-        
-        downsampling_factor_intensity = math.floor(RATE/FPS)
-        downsampling_factor_frequency = math.floor(vocals_frequency.size/CHUNKTIME/FPS)
-        
-        for i in range(FPS*CHUNKTIME):
-            for j in range(downsampling_factor_intensity):
-            
-                vocals_intensity_temp += vocals_mono_abs[i*downsampling_factor_intensity + j]
-                other_intensity_temp += vocals_mono_abs[i*downsampling_factor_intensity + j]
-                bass_intensity_temp += vocals_mono_abs[i*downsampling_factor_intensity + j]
-                drums_intensity_temp += vocals_mono_abs[i*downsampling_factor_intensity + j]
-                
-            vocals_intensity[i] = vocals_intensity_temp
-            other_intensity[i] = other_intensity_temp
-            drums_intensity[i] = drums_intensity_temp
-            bass_intensity[i] = bass_intensity_temp
-            
-            vocals_intensity_temp = 0
-            other_intensity_temp = 0
-            bass_intensity_temp = 0
-            drums_intensity_temp = 0
-            
-        vocals_frequency_temp = 0
-        other_frequency_temp = 0
-        drums_frequency_temp = 0
-        bass_frequency_temp = 0
-        
-        vocals_frequency_downsampled = np.zeros(FPS*CHUNKTIME)
-        other_frequency_downsampled = np.zeros(FPS*CHUNKTIME)
-        bass_frequency_downsampled = np.zeros(FPS*CHUNKTIME)
-        drums_frequency_downsampled = np.zeros(FPS*CHUNKTIME)
-            
-        for i in range(FPS*CHUNKTIME):
-            for j in range(downsampling_factor_frequency):
-                vocals_frequency_temp += vocals_frequency[math.floor(i*downsampling_factor_frequency) + j]
-                other_frequency_temp += vocals_frequency[math.floor(i*downsampling_factor_frequency) + j]
-                bass_frequency_temp += vocals_frequency[math.floor(i*downsampling_factor_frequency) + j]
-                drums_frequency_temp += vocals_frequency[math.floor(i*downsampling_factor_frequency) + j]
-                
-            vocals_frequency_downsampled[i] = vocals_frequency_temp / downsampling_factor_frequency 
-            other_frequency_downsampled[i] = other_frequency_temp / downsampling_factor_frequency
-            drums_frequency_downsampled[i] = drums_frequency_temp / downsampling_factor_frequency
-            bass_frequency_downsampled[i] = bass_frequency_temp / downsampling_factor_frequency
-            
-            vocals_frequency_temp = 0
-            other_frequency_temp = 0
-            drums_frequency_temp = 0
-            bass_frequency_temp = 0
-            
-            
-        print('vocals frequency max:')
-        print(np.max(vocals_mono))
-        print('other frequency max')
-        print(np.max(other_mono))
-        print('vocals_frequency length')
-        print(vocals_frequency.size)
-        
-        print('time for audio loop:')"""
-
-
-
-
+        #display performance
         time_end = tm.perf_counter()
-        print(time_end - time_begin)
+        print("audio worker finished with time: " + str(time_end - time_begin))
 
-
-    
     return
+
+
 
   
