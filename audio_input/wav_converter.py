76425f091c40a934467a2f4a7c06fc0e4db8449e
EmanueleHarrasser <85946327+EmanueleHarrasser@users.noreply.github.com>
2022-05-23 01:05:33 +0200

    Update wav_converter.py

--- audio_input/wav_converter.py
+++ audio_input/wav_converter.py
@@ -3,9 +3,9 @@ import subprocess
 
 #converts a file with the name "name" and the extension "extension_input" to a file with the same "name" and the "extension_output"
 #ffmpeg has to be installed for this to work
-def convert_file(name,extension_input,extensoin_output):
-    command = "C:\\ffmpeg\\bin\\ffmpeg -i {video} {output}".format(video="C:\\Users\\emhz\\Documents\\Python\\acuvi\\audio_input\\downloaded_files\\" + name + "." + extension_input,
-                                                  output="C:\\Users\\emhz\\Documents\\Python\\acuvi\\audio_input\\wav_files\\" + name + "." + extensoin_output)
+def convert_file(name,extension_input,extensoin_output,path):
+    command = "C:\\ffmpeg\\bin\\ffmpeg -i {video} {output}".format(video=path+"\\downloaded_files\\" + name + "." + extension_input,
+                                                  output=path+"\\wav_files\\" + name + "." + extensoin_output)
     print(command)
     subprocess.call(command,shell=True, env={'PATH': os.getenv('PATH')})
 
